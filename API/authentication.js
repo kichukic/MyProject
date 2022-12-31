45c17bfb37c4476380119fc91fe7734c2c44f341
kichukic <kichukic@gmail.com>
2022-12-31 12:38:49 +0530

    signupcommit

--- API/authentication.js
+++ API/authentication.js
@@ -1,20 +1,38 @@
+const { application } = require("express")
 const express = require("express")
 const router = express.Router()
 const mongo = require('../connection/database')
+const parse = require("body-parser")
+const { default: mongoose } = require("mongoose")
+
+
+router.use(parse.urlencoded({extended:true}))
+router.use(parse.json())
 
 
 
-router.get('/',(req,res)=>{
+router.get('/signup',(req,res)=>{
     res.render('login')
 })
 
-router.post('/signup',(req,res)=>{
+router.post('/signup',async(req,res)=>{
         const data = {
             username: req.body.username,
             email:req.body.email,
             password:req.body.password
         }
-        console.log(data)
+        const checkUserExist = await mongo.collections.findOne({email:req.body.email})
+        try {
+            if(checkUserExist === null){
+                await mongodb.collections.inserMany([data])
+                res.send("sucess fully created the account")
+            }else if(checkUserExist.email === req.body.email){
+                res.send("the user already exists")
+            }
+        } catch (error) {
+            res.send("some error occured")
+        }
+      console.log(data)
 })
 
 
--- Main.js
+++ Main.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const logger = require("morgan");
-const parse = require("body-parser")
 const path = require("path");
 const mongodb = require("./connection/database")
 const app = express()
@@ -8,12 +7,11 @@ const stableroute =require('./API/authentication');
 const { mongo } = require("mongoose");
 const pagepath = path.join(__dirname,"views");
 
-app.use("/api",stableroute)
+app.use("/",stableroute)
 app.use("mongo",mongodb)
 app.use(logger("tiny")) 
 app.use(express.static(pagepath))
-app.use(parse.urlencoded({extended:true}))
-app.use(parse.json())
+
 
 
 app.set("view engine","hbs")
